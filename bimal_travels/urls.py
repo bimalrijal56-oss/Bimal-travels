6599ec0b061219f96bad838941aa4bddb0d05dcc
bimalrijal56-oss <bimalrijal56@gmail.com>
2026-08-14 13:33:28 +0545

    Fix media paths and prepare Railway deployment

--- bimal_travels/urls.py
+++ bimal_travels/urls.py
@@ -18,7 +18,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from travels.views import *
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +29,5 @@ urlpatterns = [
     path('login', login),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
