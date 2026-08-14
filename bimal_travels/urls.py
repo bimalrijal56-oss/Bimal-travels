02be23bb7a7054bee42938ed0abe742dbcc00089
bimalrijal56-oss <bimalrijal56@gmail.com>
2026-08-14 19:18:31 +0545

    Serve media files explicitly regardless of DEBUG

--- bimal_travels/urls.py
+++ bimal_travels/urls.py
@@ -16,17 +16,16 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include, re_path
 from travels.views import *
-from django.conf.urls.static import static
 from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
-    path("api/",include('travels.urls')),   
+    path("api/", include('travels.urls')),
     path('sign-up', signup),
     path('login', login),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
